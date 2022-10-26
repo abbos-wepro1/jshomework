a5d1810fecfeeb18f6359812c2d99e47925ab0e4
abbos-wepro1 <116338606+abbos-wepro1@users.noreply.github.com>
2022-10-26 10:38:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let int = prompt('How old are u?')
+if (int % 2 === 1) {
+    console.log("non-numeric");
+} else {
+    console.log("numeral");
+}
+
+
+
+let age = prompt('random number')
+if(age >= '10') {
+    console.log('come on');
+} else {
+    console.log('go home');
+}
+
+
